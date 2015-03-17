43b73cee1c7001b7512948a44aeaff27afac28e9
Charith Hewage <charithhewage@gmail.com>
2015-03-18 01:25:00 +0530

    routes refactoring WIP

--- config/routes.rb
+++ config/routes.rb
@@ -9,27 +9,26 @@ Glitter::Application.routes.draw do
   
 
   #resources :projects
-  #resources :identities, only: [:destroy,:index]
-  #resources :comments, only: [:new, :create, :destroy]
-  #resources :glitterposts
-  #resources :notifications, only: [:index,:show]
+  resources :identities, only: [:destroy,:index]
+  resources :comments, only: [:new, :create, :destroy]
+  resources :glitterposts
+  resources :notifications, only: [:index,:show]
 
 
   scope '/:username/:projects', to: "projects" do
     resources :projects
     get :commits, to: 'projects#commits'
-    #get ':xid/commits/:tree_id' => 'projects#commits'
-    #constraints: { id: /\d.+/ }
-    
+    get :pulls,   to: 'projects#pulls'
+
     scope ':xid/' do
       get '/commits/:tree_id',  to: 'projects#commits'
       get 'commit/:tree_id',    to: 'projects#projectcommit'
-      get '/master/:image_name',to: 'projects#masterbranch', :image_name => /[^\/]*/
-      get '/master/:image_name/history', to: 'projects#file_history', :image_name => /[^\/]*/
-      get '/createsvg',               to: 'projects#new_svg'
-      get '/newfile',                 to: 'projects#newfile'
-      get '/master/:image_name/edit', to: 'projects#edit_svg', :image_name => /[^\/]*/
-      get '/master/:image_name/update',   to: 'projects#update', :image_name => /[^\/]*/
+      get '/master/:image_name',to: 'projects#masterbranch', image_name: /[^\/]*/
+      get '/master/:image_name/history', to: 'projects#file_history', image_name:  /[^\/]*/
+      get '/createsvg',                  to: 'projects#new_svg'
+      get '/newfile',                    to: 'projects#newfile'
+      get '/master/:image_name/edit',    to: 'projects#edit_svg', :image_name => /[^\/]*/
+      get '/master/:image_name/update',  to: 'projects#update', :image_name => /[^\/]*/
       delete '/master/:image_name/delete',to: 'projects#file_delete', :image_name => /[^\/]*/
       post '/follow',             to: 'projects#follow'
       get '/fork',                to: 'projects#fork'
@@ -39,7 +38,7 @@ Glitter::Application.routes.draw do
       get '/pull/:pull_id/merge', to: 'projects#merge'
       get '/pull/:pull_id/close', to: 'projects#close'
       get '/pull/:pull_id/open',  to: 'projects#open'
-      get '/pulls',               to: 'projects#pulls'
+      get '/pulls',               to: redirect('projects#pulls', status: 301)
       get '/settings',            to: 'projects#settings'
       get '/issues',              to: 'issues#index'
       get '/issue/:sub_id',       to: 'issues#show'
@@ -48,15 +47,61 @@ Glitter::Application.routes.draw do
       post '/issue/:id/close',    to: 'issues#close'
 
     end
+
+    get '/commits' => 'projects#commits'
+    get '/commit/:tree_id' => 'projects#projectcommit'
+    get '/tree/:branch/' => 'projects#show_tree_content'
+    get '/tree/:branch/*destination' => 'projects#show_tree_content'
+    get '/blob/:branch/*destination' => 'projects#show_blob_content', :destination => /.*/
+    get '/master/:image_name' => 'projects#masterbranch', :image_name => /[^\/]*/
+    get '/master/:image_name/history' => 'projects#file_history', :image_name => /[^\/]*/
+    get '/createsvg' => 'projects#new_svg'
+    get '/newfile' => 'projects#newfile'
+    get '/master/:image_name/edit' => 'projects#edit_svg', :image_name => /[^\/]*/
+    get '/master/:image_name/update' => 'projects#update', :image_name => /[^\/]*/
+    delete '/master/:image_name/delete' => 'projects#file_delete', :image_name => /[^\/]*/
+    post '/follow' => 'projects#follow'
+    get '/fork' => 'projects#fork'
+    get '/forkyou' => 'projects#forkyou'
+    get '/pull' => 'projects#pull_request'
+    get '/pull/:pull_id' => 'projects#pull'
+    get '/pull/:pull_id/merge' => 'projects#merge'
+    get '/pull/:pull_id/close' => 'projects#close'
+    get '/pull/:pull_id/open' => 'projects#open'
+    get '/pulls' => 'projects#pulls'
+    get '/settings' => 'projects#settings'
+    get '/issues/new' => 'issues#new'
+    get '/issues' => 'issues#index'
+    post '/issues/new' => 'issues#create'
+    get  '/issue/:sub_id' => 'issues#show'
+    post '/issue/:sub_id/close' => 'issues#close'
+
+
   end
 
-=begin  
+
+
+
   post 'glitterposts/:id/edit' => 'glitterposts#update'
   get '/inspire' => 'projects#index'
-  get '/dashboard' => 'dashboard#index', :as => :dashboard
+  get '/dashboard' => 'dashboard#index', :as => :dashboard\
+
   get '/:username/follow' => 'relationships#follow'
   get '/:username/unfollow' => 'relationships#unfollow'
   get '/:username' => 'users#show'
+  
+  get '/:username/projects' => 'projects#user_show'
+  get '/:username/projects/following' => 'users#list_followed_projects', :as => :followed_projects
+  get '/:username/:project' => 'projects#show'
+
+=begin  
+  -post 'glitterposts/:id/edit' => 'glitterposts#update'
+  -get '/inspire' => 'projects#index'
+  -get '/dashboard' => 'dashboard#index', :as => :dashboard
+  get '/:username/follow' => 'relationships#follow'
+  get '/:username/unfollow' => 'relationships#unfollow'
+  get '/:username' => 'users#show'
+  
   get '/:username/projects' => 'projects#user_show'
   get '/:username/projects/following' => 'users#list_followed_projects', :as => :followed_projects
   get '/:username/:project' => 'projects#show'
@@ -88,33 +133,33 @@ Glitter::Application.routes.draw do
   get '/:username/:project/issue/:sub_id' => 'issues#show'
   post '/:username/:project/issue/:sub_id/close' => 'issues#close'
 
-  get '/:username/:project/:xid' => 'projects#show'
-  get '/:username/:project/:xid/commits/:tree_id' => 'projects#commits'
-  get '/:username/:project/:xid/commit/:tree_id' => 'projects#projectcommit'
-  get '/:username/:project/:xid/master/:image_name' => 'projects#masterbranch', :image_name => /[^\/]*/
-  get '/:username/:project/:xid/master/:image_name/history' => 'projects#file_history', :image_name => /[^\/]*/
-  get '/:username/:project/:xid/createsvg' => 'projects#new_svg'
-  get '/:username/:project/:xid/newfile' => 'projects#newfile'
-  get '/:username/:project/:xid/master/:image_name/edit' => 'projects#edit_svg', :image_name => /[^\/]*/
-  get '/:username/:project/:xid/master/:image_name/update' => 'projects#update', :image_name => /[^\/]*/
-  delete '/:username/:project/:xid/master/:image_name/delete' => 'projects#file_delete', :image_name => /[^\/]*/
-  post '/:username/:project/:xid/follow' => 'projects#follow'
-  get '/:username/:project/:xid/fork' => 'projects#fork'
-  get '/:username/:project/:xid/forkyou' => 'projects#forkyou'
-  get '/:username/:project/:xid/pull' => 'projects#pull_request'
-  get '/:username/:project/:xid/pull/:pull_id' => 'projects#pull'
-  get '/:username/:project/:xid/pull/:pull_id/merge' => 'projects#merge'
-  get '/:username/:project/:xid/pull/:pull_id/close' => 'projects#close'
-  get '/:username/:project/:xid/pull/:pull_id/open' => 'projects#open'
-  get '/:username/:project/:xid/pulls' => 'projects#pulls'
-  get '/:username/:project/:xid/settings' => 'projects#settings'
-  get '/:username/:project/:xid/issues' => 'issues#index'
-  get '/:username/:project/:xid/issue/:sub_id' => 'issues#show'
-  get '/:username/:project/:xid/issues/new' => 'issues#new'
-  post '/:username/:project/:xid/issues/new' => 'issues#create'
-  post '/:username/:project/:xid/issue/:id/close' => 'issues#close'
+ - get '/:username/:project/:xid' => 'projects#show'
+ - get '/:username/:project/:xid/commits/:tree_id' => 'projects#commits'
+  -get '/:username/:project/:xid/commit/:tree_id' => 'projects#projectcommit'
+  -get '/:username/:project/:xid/master/:image_name' => 'projects#masterbranch', :image_name => /[^\/]*/
+  -get '/:username/:project/:xid/master/:image_name/history' => 'projects#file_history', :image_name => /[^\/]*/
+  -get '/:username/:project/:xid/createsvg' => 'projects#new_svg'
+  -get '/:username/:project/:xid/newfile' => 'projects#newfile'
+  -get '/:username/:project/:xid/master/:image_name/edit' => 'projects#edit_svg', :image_name => /[^\/]*/
+  -get '/:username/:project/:xid/master/:image_name/update' => 'projects#update', -:image_name => /[^\/]*/
+  -delete '/:username/:project/:xid/master/:image_name/delete' => 'projects#file_delete', :image_name => /[^\/]*/
+  -post '/:username/:project/:xid/follow' => 'projects#follow'
+  -get '/:username/:project/:xid/fork' => 'projects#fork'
+  -get '/:username/:project/:xid/forkyou' => 'projects#forkyou'
+  -get '/:username/:project/:xid/pull' => 'projects#pull_request'
+  -get '/:username/:project/:xid/pull/:pull_id' => 'projects#pull'
+  -get '/:username/:project/:xid/pull/:pull_id/merge' => 'projects#merge'
+  -get '/:username/:project/:xid/pull/:pull_id/close' => 'projects#close'
+  -get '/:username/:project/:xid/pull/:pull_id/open' => 'projects#open'
+  -get '/:username/:project/:xid/pulls' => 'projects#pulls'
+  -get '/:username/:project/:xid/settings' => 'projects#settings'
+  -get '/:username/:project/:xid/issues' => 'issues#index'
+  -get '/:username/:project/:xid/issue/:sub_id' => 'issues#show'
+  -get '/:username/:project/:xid/issues/new' => 'issues#new'
+  -post '/:username/:project/:xid/issues/new' => 'issues#create'
+  -post '/:username/:project/:xid/issue/:id/close' => 'issues#close'
 
-  
+=end  
   resources :projects do
     member do
       get 'projects/:id/invite.xml' => 'projects#invite'
@@ -126,5 +171,5 @@ Glitter::Application.routes.draw do
       post :update_svg, :as => :update_svg
     end
   end 
-=end
+
 end
